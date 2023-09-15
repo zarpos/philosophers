aefcd40416f36c8baba6c89618db76eb5e0b1e1b
Diego Rubio morato <diegorubiomorato@gmail.com>
2023-09-15 17:03:24 +0200

    a

--- philo/thread_practice.c
+++ philo/thread_practice.c
@@ -6,28 +6,34 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:22:09 by drubio-m          #+#    #+#             */
-/*   Updated: 2023/09/14 12:40:35 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/09/15 16:15:45 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-	int i = 1;
+	int k  = 0;
 
 void *routine()
 {
-
-	printf("hola que tal soy el philo: %d\n", i++);
+	k += 1;
 	return NULL;
 }
 
 void	ft_example(void)
 {
-	pthread_t	th[1000000];
 	int i = 0;
-	while (i++ < 1000000)
+	pthread_t	th[10];
+	while (i++ < 10)
 	{
 		pthread_create(th + i, NULL, &routine, NULL);
-		pthread_join(th[i], NULL);
+		printf("Philo number :%d Has been created\n", i);
 	}
+	i = 0;
+	while (i++ < 10)
+	{
+		pthread_join(th[i], NULL);
+		printf("Philo number :%d Has been joined\n", i);
+	}
+	printf("%d", k);
 }
