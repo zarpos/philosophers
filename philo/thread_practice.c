15defa7b2e112c68b058c9b2e0476a99b352606b
Diego Rubio morato <diegorubiomorato@gmail.com>
2023-09-17 21:09:45 +0200

    thread loop

--- philo/thread_practice.c
+++ philo/thread_practice.c
@@ -6,16 +6,18 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:22:09 by drubio-m          #+#    #+#             */
-/*   Updated: 2023/09/15 16:15:45 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/09/16 19:33:58 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 	int k  = 0;
-
-void *routine()
+	int primes[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+void *routine(void *arg)
 {
+	int index = *(int *)arg;
+	printf("Me ha tocado el número : %d\n", primes[index]);
 	k += 1;
 	return NULL;
 }
@@ -26,7 +28,7 @@ void	ft_example(void)
 	pthread_t	th[10];
 	while (i++ < 10)
 	{
-		pthread_create(th + i, NULL, &routine, NULL);
+		pthread_create(th + i, NULL, &routine, &i);
 		printf("Philo number :%d Has been created\n", i);
 	}
 	i = 0;
