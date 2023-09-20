fb429db721245e5befab6ca1cf19a47f1de6887a
Diego Rubio morato <diegorubiomorato@gmail.com>
2023-09-20 21:04:22 +0200

    b

--- philo/thread_practice.c
+++ philo/thread_practice.c
@@ -6,7 +6,7 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 17:22:09 by drubio-m          #+#    #+#             */
-/*   Updated: 2023/09/20 11:33:55 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/09/20 20:31:35 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,21 @@ void	init_philos(t_data	*data)
 	while (i < data->number_of_philosophers)
 	{
 		philos[i].id = i + 1;
+		printf("Id del philo: %d\n", i);
 		philos[i].meal_counter = 0;
+		printf("Meal count: %d\n", philos[i].meal_counter);
 		philos[i].status = 0;
+		printf("Estatus del philo: %d\n", philos[i].status);
 		philos[i].last_meal = 0;
+		printf("Last meal del philo: %llu\n", philos[i].last_meal);
 		philos[i].left_fork = NULL;
+		printf("Dirección del left fork: %p\n", (void *)philos[i].left_fork);
 		philos[i].right_fork = NULL;
+		printf("Dirección del right fork: %p\n", (void *)philos[i].right_fork);
 		philos[i].print = NULL;
+		printf("Dirección del print: %p\n", (void *)philos[i].print);
 		philos[i].lock = NULL;
+		printf("Dirección del lock: %p\n", (void *)philos[i].lock);
 		philos[i].data = data;
 		i++;
 	}
